33c12e17e21108cc832f2b5d2fefc79d833a2c0c
lilimelgar <lmelgares@runbox.com>
2025-03-13 16:58:02 +0100

    Update file.py
    adds function run_work_for_record

--- vocab/util/file.py
+++ vocab/util/file.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 import logging
+import requests
 
 from typing import Generator, Tuple
 from contextlib import contextmanager
@@ -9,8 +10,13 @@ from contextlib import contextmanager
 from vocab.util.xml import write_xml, read_xml
 from vocab.util.redis import get_object_redis, store_object_redis, delete_object_redis
 
+from pydantic import BaseModel
+from vocab.app import celery
+from vocab.util.http import session
+
 log = logging.getLogger(__name__)
 
+editor_api_record = 'http://localhost:1210'
 
 @contextmanager
 def run_work_for_file(file: str) -> Generator[Tuple[int, int], None, None]:
@@ -24,6 +30,8 @@ def run_work_for_file(file: str) -> Generator[Tuple[int, int], None, None]:
     log.info(f"Start work for {file} with nr {nr} and id {id}")
     yield nr, id
 
+    log.info(f"Store xml for {file} with nr {nr} and id {id}")
+
     xml = read_xml(get_object_redis(nr, id))
     with open(file, 'wb') as f:
         f.write(write_xml(xml, True))
@@ -31,6 +39,28 @@ def run_work_for_file(file: str) -> Generator[Tuple[int, int], None, None]:
     delete_object_redis(nr, id)
     log.info(f"Finished work for {file} with nr {nr} and id {id}")
 
+@contextmanager
+def run_work_for_record(nr: int) -> Generator[int, None, None]:
+    id = int(time.time())
+#   record = requests 'http://localhost:1210/app/vocabs/profile/clarin.eu%3Acr1%3Ap_1653377925723/record/{nr}' ## here we will call the API with a Get request to the Editor, only add the number
+#   Figure out how to change the accept header with request (to xml instead of json)
+    response = session.get(f"{editor_api_record}/app/vocabs/profile/clarin.eu%3Acr1%3Ap_1653377925723/record/{nr}",
+                         headers={"accept":"application/xml"})
+
+    store_object_redis(nr, id, response.content)
+
+    log.info(f"Start work for {nr} with id {id}")
+    yield id
+
+    log.info(f"Store xml for {nr} with id {id}")
+
+    xml = read_xml(get_object_redis(nr, id))
+    # here comes PUT request to editor (/app/{app}/profile/{prof}/record/{nr})
+    update_record = session.put(f"{editor_api_record}/app/vocabs/profile/clarin.eu%3Acr1%3Ap_1653377925723/record/{nr}",
+                                body=xml, headers={"content-type":"application/xml"})
+
+    delete_object_redis(nr, id)
+    log.info(f"Finished work for {nr} with id {id}")
 
 def get_files_in_path(path: str) -> [str]:
     if os.path.isfile(path):
